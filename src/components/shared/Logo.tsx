95deb8a017a57031042996f55ace65a97ffd9dc0
khalid shaikh <khalids15704@gmail.com>
2025-08-07 09:15:59 +0000

    can you replace the logo with 64x64.png

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 font-bold text-lg font-headline">
-      <Image src="/images/32x32.png" alt="Winnova Logo" width={32} height={32} />
+      <Image src="/images/64x64.png" alt="Winnova Logo" width={32} height={32} />
       <span>Winnova</span>
     </Link>
   );
